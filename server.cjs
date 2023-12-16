7a03550d2235a43f4ca0e315ce3602e269d4323f
idkdu <idk.duderc@gmail.com>
2023-12-16 12:20:46 -0800

    Added Vite Files

---
+++ server.cjs
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the "dist" directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Handle all routes by sending the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, distDir, 'dist/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
